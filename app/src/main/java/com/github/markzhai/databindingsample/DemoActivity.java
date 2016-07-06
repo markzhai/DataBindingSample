9b7a2cb6ffb849dac97000c7988fc3ddebf60de9
markzhai <mark.zhai@morecruit.cn>
2016-07-07 00:46:27 +0800

    add expresstion example

--- app/src/main/java/com/github/markzhai/databindingsample/DemoActivity.java
+++ app/src/main/java/com/github/markzhai/databindingsample/DemoActivity.java
@@ -1,9 +1,10 @@
 package com.github.markzhai.databindingsample;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.adapters.TextViewBindingAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.markzhai.databindingsample.databinding.ActivityDemoBinding;
 
@@ -11,18 +12,29 @@ public class DemoActivity extends AppCompatActivity {
 
     Employee employee = new Employee("Zhai", "Mark");
 
+    ActivityDemoBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityDemoBinding binding =
-                DataBindingUtil.setContentView(this, R.layout.activity_demo);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_demo);
         binding.setEmployee(employee);
-        binding.setWatcher(new TextViewBindingAdapter.OnTextChanged() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                employee.setFirstName(s.toString());
-                binding.setEmployee(employee);
-            }
-        });
+        binding.setPresenter(new Presenter());
+    }
+
+    public class Presenter {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            employee.setFirstName(s.toString());
+            binding.setEmployee(employee);
+        }
+
+        public void onClick(View view) {
+            Toast.makeText(DemoActivity.this, "点到了", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onClickListenerBinding(Employee employee) {
+            Toast.makeText(DemoActivity.this, employee.getLastName(),
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
