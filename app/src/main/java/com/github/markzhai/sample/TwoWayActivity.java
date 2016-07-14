7d5ed6e737174ad53b6faa1ba89d81cfec09803c
markzhai <mark.zhai@morecruit.cn>
2016-07-14 21:19:49 +0800

    show usage of addOnPropertyChangedCallback

--- app/src/main/java/com/github/markzhai/sample/TwoWayActivity.java
+++ app/src/main/java/com/github/markzhai/sample/TwoWayActivity.java
@@ -1,11 +1,14 @@
 package com.github.markzhai.sample;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.github.markzhai.sample.databinding.ActivityTwoWayBinding;
+import com.github.markzhai.sample.BR;
 
 /**
  * @author markzhai on 16/7/7
@@ -15,17 +18,33 @@ public class TwoWayActivity extends AppCompatActivity {
 
     ActivityTwoWayBinding mBinding;
 
+    FormModel mModel = new FormModel("markzhai", "123456");
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_two_way);
         mBinding.setPresenter(new Presenter());
-        mBinding.setModel(new FormModel("markzhai", "123456"));
+
+        mBinding.setModel(mModel);
+        mModel.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable observable, int i) {
+                if (i == BR.name) {
+                    Toast.makeText(TwoWayActivity.this, "name changed",
+                            Toast.LENGTH_SHORT).show();
+                } else if (i == BR.password) {
+                    Toast.makeText(TwoWayActivity.this, "password changed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public class Presenter {
         public void save(FormModel model) {
-
+            Toast.makeText(TwoWayActivity.this, "saved",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }
